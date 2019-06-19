9853d28ea4077c80e09a8eced29c22dafaac6cce
NGnius (Graham) <ngniusness@gmail.com>
2019-06-19 12:22:29 -0400

    Fix endless search and bad char save bugs

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def main(args):
         if calls % CALLS_BEFORE_REFRESH == 0 and args.batch:
             print('Refreshing token...')
             token = do_login(args)['Token']
-        save_path = join(args.out, bot['itemName']+'.'+args.extension.lstrip('.'))
+        save_path = join(args.out, remove_bad_chars(bot['itemName']+'-'+str(bot['itemId']))+'.'+args.extension.lstrip('.'))
         print('Downloading %s to %s...' % (bot['itemName'], save_path))
         bot_info = factory.factory_bot(token, bot['itemId'])
         with open(save_path, 'w') as f:
             json.dump(bot_info, f, indent=4)
         if args.thumbnail is True:
             # this in an AWS CDN, idc about spamming it
-            threads.append(Thread(target=save_thumbnail, args=(bot_info['name'],bot_info['thumbnail'])))
+            threads.append(Thread(target=save_thumbnail, args=(bot_info['name']+'-'+str(bot['itemId']),bot_info['thumbnail'], args)))
             threads[-1].start()
         if calls == args.max and args.max >= 0:
             break
@@ -56,24 +56,30 @@ def do_login(args):
         print('! Password and username must both be present or both missing.')
         return
 
-def save_thumbnail(name, url):
+def save_thumbnail(name, url, args):
     thumbnail_img = factory.factory_thumbnail(url)
-    thumbnail_path = join(args.out, name+'.jpg')
+    thumbnail_path = join(args.out, remove_bad_chars(name)+'.jpg')
     print('Downloading %s image to %s...' % (name, thumbnail_path))
     with open(thumbnail_path, 'wb') as f:
         f.write(thumbnail_img)
 
+def remove_bad_chars(path, replacement=''):
+    NONO_CHARS = ['\\', '/', '?', ';', '*', '"', '<', '>', '|']
+    for c in NONO_CHARS:
+        path = path.replace(c, replacement)
+    return path
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('search', metavar='SEARCH', type=str,
+                        default='', help='The search text (REQUIRED!)')
     parser.add_argument('--password', '-p', metavar='PASSWORD', type=str,
-                        default=None, help='Your password to login to Robocraft')
+                        default=None, help='Your password to login to Robocraft (optional)')
     parser.add_argument('--username', '-u', metavar='USERNAME', type=str,
-                        default=None, help='Your username to login to Robocraft')
-    parser.add_argument('--search', metavar='TEXT', type=str,
-                        default='', help='The search text')
+                        default=None, help='Your username to login to Robocraft (optional)')
     parser.add_argument('--out', '-o' , metavar='PATH', type=str,
-                        default='./', help='Output folder path')
+                        default='./', help='Output folder path (optional, default: current folder)')
     parser.add_argument('--batch', '-l', action='store_true',
                         default=False, help='Handle chores for longer time periods. If errors occur after a minute or two, try adding this')
     parser.add_argument('--player', action='store_true',
@@ -81,7 +87,7 @@ if __name__ == '__main__':
     parser.add_argument('--extension', metavar='EXT', type=str,
                         default='bot', help='File extension to use to store the plaintext files')
     parser.add_argument('--max', metavar='N', type=int,
-                        default=-1, help='Maximum bots to download')
+                        default=-1, help='Maximum bots to download (optional)')
     parser.add_argument('--thumbnail', '-i', action='store_true',
                         default=False, help='Download the robot thumbnail too')
 
